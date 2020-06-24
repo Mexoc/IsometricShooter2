3401a6d92fc0389c51f5572fba421269eb509f96
Mexoc <43862015+Mexoc@users.noreply.github.com>
2020-06-24 16:10:24 +0300

    made script for volume slider

--- IsometricShooter2/Assets/Scripts/UI/SoundSliderVolume.cs
+++ IsometricShooter2/Assets/Scripts/UI/SoundSliderVolume.cs
@@ -1,18 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SoundSliderVolume : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private AudioSource[] audioSources;
+    private float volume;
+
     void Start()
     {
-        
+        audioSources = FindObjectsOfType<AudioSource>();
+        volume = 1;
+        gameObject.GetComponent<Slider>().value = volume;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        SetVolumes();
+    }
+
+    private void SetVolumes()
+    {
+        volume = gameObject.GetComponent<Slider>().value;
+        foreach (AudioSource source in audioSources)
+        {
+            source.volume = volume;
+        }
     }
 }
