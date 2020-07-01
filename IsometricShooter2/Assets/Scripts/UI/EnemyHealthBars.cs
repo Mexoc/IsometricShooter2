7ca9da278119e3f89bdbc083ba57d206ec655c36
Mexoc <43862015+Mexoc@users.noreply.github.com>
2020-07-01 19:41:11 +0300

    basic build

---
+++ IsometricShooter2/Assets/Scripts/UI/EnemyHealthBars.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealthBars : MonoBehaviour
+{
+    private Canvas enemyHealthBar;
+    private GameObject mainCamera;
+
+    private void Awake()
+    {
+        enemyHealthBar = gameObject.GetComponentInChildren<Canvas>();
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+    }
+
+    private void Update()
+    {
+        HealthbarCameraLook();
+    }
+
+    private void HealthbarCameraLook()
+    {
+        enemyHealthBar.transform.LookAt(mainCamera.transform);
+    }
+
+}
